45909a71f2bc91ae30187930581ac6effc1020bc
Mahdi Jaffery <mahdijaffri5@gmail.com>
2025-01-21 20:40:54 +0500

    Route Transcripts

--- backend/routes/transcriptRoutes.js
+++ backend/routes/transcriptRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 
+const { getTranscript } = require("../controllers/transcriptController");
 const { checkAuthorization } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
+
+router.get("/", checkAuthorization, getTranscript);
 
 module.exports = router;
