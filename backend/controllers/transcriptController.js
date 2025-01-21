0bba12ace9c6c1bd95c0a8e8f9053692c91339d9
Mahdi Jaffery <mahdijaffri5@gmail.com>
2025-01-21 20:36:15 +0500

    GET Transcripts

--- backend/controllers/transcriptController.js
+++ backend/controllers/transcriptController.js
@@ -1,1 +1,23 @@
 const pool = require("../config/database");
+
+const getTranscript = async (request, response) => {
+  const {
+    session: {
+      user: { userid },
+    },
+  } = request;
+
+  try {
+    let res = await pool.query(`Select * from Transcript where userid = $1`, [
+      userid,
+    ]);
+
+    if (!res.rowCount)
+      return response.status(404).send(`No Transcript Available`);
+    
+    response.status(200).json(res.rows);
+  } catch (error) {
+    console.error("Error in getTranscript:", error);
+    return response.status(500).send("Internal Server Error");
+  }
+};
