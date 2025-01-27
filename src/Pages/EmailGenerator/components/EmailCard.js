b2042258649768a7e527a454d3aa07a11cb5c5e1
Aliyan18 <rajaaliyanahmed@gmail.com>
2025-01-27 15:13:37 -0800

    email and app generation

---
+++ src/Pages/EmailGenerator/components/EmailCard.js
@@ -0,0 +1,21 @@
+
+export default function EmailCard({data:{title, description,image}, setShowEmailForm}){
+
+
+
+    const styles={
+    //    width:'40%' if width of each card is to be manualy adjust use this
+    }
+        return (
+            <div
+                            className="card"
+                            onClick={() => setShowEmailForm(true)}
+                        >
+                            <img src={image} alt="Card" />
+                            <div className="card-footer">
+                                <h4>{title}</h4>
+                                <p>{description}</p>
+                            </div>
+                        </div>
+        )
+    }
