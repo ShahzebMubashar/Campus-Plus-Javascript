bd5f8c96b335e3f575010dc02cfbf5ab78836c62
Mahdi Jaffery <mahdijaffri5@gmail.com>
2025-04-21 20:17:06 +0500

    Display of posted_at improved

--- src/Pages/Chatroom/components/RoomView.js
+++ src/Pages/Chatroom/components/RoomView.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Navigate } from "react-router-dom";
 
 export default function RoomView({ room, onBack, onLeave }) {
   const [posts, setPosts] = useState([]);
