e690c17b0a927ec17bec9593467fa7d43eb8142e
ShahzebMubashar <shahzebmubashar7@gmail.com>
2025-04-24 21:14:44 +0500

    adding sh faculty

--- src/Pages/Faculty/Faculty.js
+++ src/Pages/Faculty/Faculty.js
@@ -123,7 +123,7 @@ const IntroSection = () => (
 
 const SchoolsContainer = () => (
     <div className="schools-container">
-        {['EE', 'CV', 'CS', 'M', 'SH'].map((school, index) => (
+        {['Electrial Engineering', 'Civil Engineering', 'Computer Science', 'Management', 'Science & Humanities'].map((school, index) => (
             <a href={`#${school}`} key={index}>
                 <div className="school-box">{`FAST School of ${school}`}</div>
             </a>
