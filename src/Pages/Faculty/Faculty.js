fc232ee8d118c3560ea0f52ff3e112ae349e6f95
ShahzebMubashar <shahzebmubashar7@gmail.com>
2025-04-24 21:19:01 +0500

    updating faculty

--- src/Pages/Faculty/Faculty.js
+++ src/Pages/Faculty/Faculty.js
@@ -123,7 +123,7 @@ const IntroSection = () => (
 
 const SchoolsContainer = () => (
     <div className="schools-container">
-        {['Electrial Engineering', 'Civil Engineering', 'Computer Science', 'Management', 'Science & Humanities'].map((school, index) => (
+        {['EE', 'CV', 'CS', 'M', 'SH'].map((school, index) => (
             <a href={`#${school}`} key={index}>
                 <div className="school-box">{`FAST School of ${school}`}</div>
             </a>
