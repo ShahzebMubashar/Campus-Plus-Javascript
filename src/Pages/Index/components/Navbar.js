ff70766747d697d3e07d48b82b0df30f774ec4bf
ShahzebMubashar <shahzebmubashar7@gmail.com>
2025-05-02 21:42:35 +0500

    Changing navbar

--- src/Pages/Index/components/Navbar.js
+++ src/Pages/Index/components/Navbar.js
@@ -3,6 +3,14 @@ import './Navbar.css';
 import Logo from '../cp_logo.png';
 import { Link, useNavigate } from 'react-router-dom';
 import { AiOutlineDown, AiOutlineHeart, AiOutlineFileText, AiOutlineHome, AiOutlineLaptop } from 'react-icons/ai';
+import { FaUserCircle } from 'react-icons/fa';
+
+
+const [showUserMenu, setShowUserMenu] = useState(false);
+
+const toggleUserMenu = () => {
+    setShowUserMenu(!showUserMenu);
+};
 
 function Navbar() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -176,19 +184,21 @@ function Navbar() {
                         </li>
                         {isLoggedIn ? (
                             <>
-                                <li><Link to="/dashboard">Dashboard</Link></li>
                                 <li className="navbaruser-dropdown">
-                                    <div className="navbaruser-container">
-                                        <div className="navbaruser-icon">
-                                            <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="User" />
-                                        </div>
-                                        <div className="navbaruser-dropdown-menu">
-                                            <Link to="/edit-profile">Edit Profile</Link>
-                                            <Link to="/settings">Settings</Link>
-                                            <span onClick={handleLogout} style={{ cursor: "pointer" }}>Logout</span>
-                                        </div>
+
+                                    <div className="navbaruser-icon">
+                                        <FaUserCircle size={32} />
                                     </div>
+
+                                    <div className="navbaruser-dropdown-menu">
+                                        <Link to="/profile">Edit Profile</Link>
+                                        <Link to="/settings">Settings</Link>
+                                        <span onClick={handleLogout} style={{ cursor: 'pointer' }}>Logout</span>
+                                    </div>
+
+
                                 </li>
+
 
                             </>
                         ) : (
