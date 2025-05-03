f263623833c5093d73780aad5ac26ce9f8db97a4
Mahdi Jaffery <mahdijaffri5@gmail.com>
2025-05-03 23:59:19 +0500

    Add Reminders on Dashboard

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const { viewUserInfo, editUserInfo, currentCourses } = require("../controllers/userController");
+const {
+  viewUserInfo,
+  editUserInfo,
+  currentCourses,
+  getReminders,
+  addReminder,
+} = require("../controllers/userController");
 const { checkAuthorisation } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
@@ -7,5 +13,7 @@ const router = express.Router();
 router.get("/profile", checkAuthorisation, viewUserInfo);
 router.put("/profile", checkAuthorisation, editUserInfo);
 router.get("/current-courses", checkAuthorisation, currentCourses);
+router.get("/my-reminders", checkAuthorisation, getReminders);
+router.post("/add-reminder", checkAuthorisation, addReminder);
 
 module.exports = router;
