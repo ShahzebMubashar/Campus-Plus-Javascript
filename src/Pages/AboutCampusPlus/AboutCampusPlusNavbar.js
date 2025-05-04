74eb0b28c5efd8595e41482c44e0bc4e4dc9d4d0
ShahzebMubashar <shahzebmubashar7@gmail.com>
2025-05-04 19:15:34 +0500

    Fixing aboutcampusplus

--- src/Pages/AboutCampusPlus/AboutCampusPlusNavbar.js
+++ src/Pages/AboutCampusPlus/AboutCampusPlusNavbar.js
@@ -3,19 +3,19 @@ import { Link } from 'react-scroll'; // Importing Link from react-scroll
 
 const Navbar = () => {
     return (
-        <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-white" data-aos="fade-down">
-            <div className="container">
-                <a className="navbar-brand" href="/">Campus+</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+        <nav className="aboutcp-navbar fixed-top navbar-expand-lg navbar-light bg-white" data-aos="fade-down">
+            <div className="aboutcp-container container">
+                <a className="aboutcp-navbar-brand navbar-brand" href="/">Campus+</a>
+                <button className="aboutcp-navbar-toggler navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#aboutCpNavbarNav" aria-controls="aboutCpNavbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="aboutcp-navbar-toggler-icon navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ms-auto">
+                <div className="aboutcp-navbar-collapse collapse navbar-collapse" id="aboutCpNavbarNav">
+                    <ul className="aboutcp-navbar-nav navbar-nav ms-auto">
                         {['Home', 'About', 'Features', 'Founder', 'Contributors', 'Reviews', 'Contact'].map((section) => (
-                            <li className="nav-item" key={section}>
+                            <li className="aboutcp-nav-item nav-item" key={section}>
                                 {/* Use Link from react-scroll for smooth scrolling */}
                                 <Link
-                                    className="nav-link"
+                                    className="aboutcp-nav-link nav-link"
                                     to={section.toLowerCase()} // Linking to the ID of the section
                                     spy={true} // Spy on scroll
                                     smooth={true} // Enable smooth scrolling
