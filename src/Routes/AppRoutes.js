832af5025d75868e9b21e611cfe7af9d9b9be29f
ShahzebMubashar <shahzebmubashar7@gmail.com>
2025-07-04 17:05:00 +0500

    added our events page

--- src/Routes/AppRoutes.js
+++ src/Routes/AppRoutes.js
@@ -26,6 +26,7 @@ import ToDo from '../Pages/ToDoList/ToDo.js'
 import Profile from '../Pages/Profile/ProfilePage.js'
 import Transcript from '../Pages/Transcripts/Transcripts.js'
 import Navbar from '../Pages/Index/components/Navbar.js';
+import OurEvents from '../Pages/OurEvents/OurEvents.js';
 
 
 function AppRoutes() {
@@ -76,6 +77,7 @@ function AppRoutes() {
                 <Route path="/profile" element={<Profile />} />
                 <Route path="/transcript" element={<Transcript />} />
                 <Route path="/navbar" element={<Navbar />} />
+                <Route path="/ourevents" element={<OurEvents />} />
             </Routes>
         </>
     );
