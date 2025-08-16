e977baa4c5c01383518b70c40f7461df573f9d63
Mahdi Jaffery <mahdijaffri5@gmail.com>
2025-08-17 01:37:12 +0500

    Finished with Notifications Page

--- backend/routes/notificationRoutes.js
+++ backend/routes/notificationRoutes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const { jwtAuthMiddleware, requireAdmin } = require('../middlewares/jwtAuthMiddleware');
 
-const { generateNotification, getNotifications } = require('../controllers/notificationsController');
+const { generateNotification, getNotifications, deleteNotification } = require('../controllers/notificationsController');
 
 router.post('/generate', jwtAuthMiddleware, requireAdmin, generateNotification);
 router.get('/', jwtAuthMiddleware, getNotifications);
+router.delete('/delete/:notificationid', jwtAuthMiddleware, deleteNotification);
 
 module.exports = router;
