0d1faa84f613b49c1c98589aec51f031ae4426d5
ShahzebMubashar <shahzebmubashar7@gmail.com>
2025-09-20 03:45:27 +0500

    vercel analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.js";
 import reportWebVitals from "./reportWebVitals.js";
+import { Analytics } from "@vercel/analytics/react"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
